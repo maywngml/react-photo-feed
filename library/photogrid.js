9cef9abc9fb9369bc26e47c4ff9a254faf46be02
maywngml <maywngml@naver.com>
2021-07-18 17:03:48 +0900

    [Update] getFullScreenImage 다시 변경

--- library/photogrid.js
+++ library/photogrid.js
@@ -389,29 +389,13 @@
                     onClick: e.lightBox_clickHandler,
                   },
                   r.map(function (n, r) {
-                    // if (n.isVideo) {
-                    //   return s.default.createElement("video", {
-                    //     key: n.id,
-                    //     // src: n.bigSrc,
-                    //     controls: true,
-                    //     className: "opaque",
-                    //     onClick: e.fullScreenImage_clickHandler
-                    //   },
-                    //     s.default.createElement("source", {
-                    //     src: n.bigSrc,
-                    //     type: "video/mp4"
-                    //     })
-                    //   )
-                    // }
-                    // else {
-                      return s.default.createElement("img", {
+                    return s.default.createElement("img",
+                      {
                         key: n.id,
                         src: n.bigSrc,
                         className: n.bigSrc == t ? "opaque" : "",
-                        onClick:
-                          n.bigSrc == t ? e.fullScreenImage_clickHandler : null,
-                      });
-                    // }
+                        onClick: n.bigSrc == t ? e.fullScreenImage_clickHandler : null
+                      })
                   })
                 );
               }),
